a77e10da671ab7c9982fed1e9960daa29dd66acd
Yankee Knight <116320996+YankeeKnight@users.noreply.github.com>
2023-04-16 12:29:17 -0400

    update connection.js

--- book-search/server/config/connection.js
+++ book-search/server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+    useNewURLParser: true,
+    useUnifiedTopoloy: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
