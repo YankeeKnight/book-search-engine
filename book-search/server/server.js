e90672ea5cc3bc1bda8be97e48880b4c7643b88f
Yankee Knight <116320996+YankeeKnight@users.noreply.github.com>
2023-04-16 16:13:01 -0400

    updated server.js

--- book-search/server/server.js
+++ book-search/server/server.js
@@ -3,8 +3,14 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware, persistedQueries: false });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +20,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on Port: ${PORT}!`);
+      console.log(`Use GraphQL at http://localhose:${PORT}${server.graphqlPath}`);
+    });
+  });
+};
+
+startApolloServer(typeDefs, resolvers);
