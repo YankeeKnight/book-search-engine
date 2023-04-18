312d3c8e571fbd5346bb4b974a58fa59055a0a54
Yankee Knight <116320996+YankeeKnight@users.noreply.github.com>
2023-04-18 17:50:32 -0400

    bug fix try

--- server/server.js
+++ server/server.js
@@ -16,9 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+
+// }
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
